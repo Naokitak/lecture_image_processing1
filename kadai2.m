18c6219e64d59620d7c62025867860aace154d74
高橋尚紀 <30608915+Naokitak@users.noreply.github.com>
2018-01-14 02:24:53 +0900

    Update and rename kadai2.md to kadai2.m

---
+++ kadai2.m
@@ -0,0 +1,11 @@
+![原画像](https://github.com/Naokitak/lecture_image_processing1/blob/master/21.png?raw=true)
+図1：
+
+![原画像](https://github.com/Naokitak/lecture_image_processing1/blob/master/22.png?raw=true)
+図2：２階調画像
+
+![原画像](https://github.com/Naokitak/lecture_image_processing1/blob/master/23.png?raw=true)
+図3：３階調画像
+
+![原画像](https://github.com/Naokitak/lecture_image_processing1/blob/master/24.png?raw=true)
+図４：４階調画像
